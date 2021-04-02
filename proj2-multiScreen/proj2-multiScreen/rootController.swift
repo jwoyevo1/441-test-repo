5d9745f6e9555ccbca4016adb20b5673858c1d51
James Woyevodsky <jameswoyevodsky@gmail.com>
2021-04-02 11:15:13 -0400

    second commit

---
+++ proj2-multiScreen/proj2-multiScreen/rootController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  proj2-multiScreen
+//
+//  Created by James Woyevodsky on 3/22/21.
+//  Copyright © 2021 Binghamton University. All rights reserved.
+//
+
+import UIKit
+
+class rootController: UIViewController {
+    
+    var firstViewController: FirstViewController?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func switchViews(_ sender: UIButton){
+        
+    }
+
+
+
+
+
+}
+
